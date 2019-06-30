2b99555936e1b361ceb93f2dae8deacbbfcf58fd
xoppel <16063289+xoppel@users.noreply.github.com>
2019-06-29 21:07:28 -0400

    More tweaks to Instant60 and switch Zeal60 to 7u Tsangan

--- keyboards/zeal60/keymaps/xoppel/config.h
+++ keyboards/zeal60/keymaps/xoppel/config.h
@@ -2,15 +2,20 @@
 
 /* enable/disable LEDs based on layout */
 #undef RGB_BACKLIGHT_USE_SPLIT_BACKSPACE
-#define RGB_BACKLIGHT_USE_SPLIT_BACKSPACE 1
+#define RGB_BACKLIGHT_USE_SPLIT_BACKSPACE 0
+
 #undef RGB_BACKLIGHT_USE_SPLIT_LEFT_SHIFT
-#define RGB_BACKLIGHT_USE_SPLIT_LEFT_SHIFT 1
+#define RGB_BACKLIGHT_USE_SPLIT_LEFT_SHIFT 0
+
 #undef RGB_BACKLIGHT_USE_SPLIT_RIGHT_SHIFT
 #define RGB_BACKLIGHT_USE_SPLIT_RIGHT_SHIFT 1
+
 #undef RGB_BACKLIGHT_USE_7U_SPACEBAR
-#define RGB_BACKLIGHT_USE_7U_SPACEBAR 0
+#define RGB_BACKLIGHT_USE_7U_SPACEBAR 1
+
 #undef RGB_BACKLIGHT_USE_ISO_ENTER
-#define RGB_BACKLIGHT_USE_ISO_ENTER 1
+#define RGB_BACKLIGHT_USE_ISO_ENTER 0
+
 #undef RGB_BACKLIGHT_DISABLE_HHKB_BLOCKER_LEDS
 #define RGB_BACKLIGHT_DISABLE_HHKB_BLOCKER_LEDS 0
 
@@ -26,5 +31,21 @@
 #define PRODUCT         USB Keyboard
 #undef DESCRIPTION
 #define DESCRIPTION     K120 Keyboard
+
+// Set up RGB underglow.
+#define RGB_DI_PIN B0          // The pin your RGB strip is wired to
+//#define RGBLIGHT_ANIMATIONS    // Require for fancier stuff (not compatible with audio)
+#define RGBLIGHT_EFFECT_ALTERNATING
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLED_NUM 34          // Number of LEDs
+#define RGBLIGHT_HUE_STEP 5    // How much each press of rgb_hue changes hue
+#define RGBLIGHT_SAT_STEP 10   // How much each press of rgb_sat changes sat
+#define RGBLIGHT_VAL_STEP 10   // How much each press of rgb_val changes val
+
+// Enable current limiting for RGB underglow.
+#define RGBSTRIP_CURRENT_LIMIT 400    // Strip current limit in mA. (USB amperage - 500mA for keyboard)
+#define RGBSTRIP_MAX_CURRENT_PER_LIGHT 50 // mA per light when at max brightness.
 
 #define PREVENT_STUCK_MODIFIERS
