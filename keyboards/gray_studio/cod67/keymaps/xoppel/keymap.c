b984f77e92d43da778eb35033573c8835671f8c6
xoppel <16063289+xoppel@users.noreply.github.com>
2019-10-06 09:41:13 -0400

    Major rework of COD67; RGB working but backlight and keymap need fixing

--- keyboards/gray_studio/cod67/keymaps/xoppel/keymap.c
+++ keyboards/gray_studio/cod67/keymaps/xoppel/keymap.c
@@ -16,31 +16,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 //		KC_TRNS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 //		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+//							     KC_SPC			KC_BSPC
 
 // layer 0 - main
         LAYOUT(
-                KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
+                KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  xxxxxxx, \
                 KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
                 KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, xxxxxxx, KC_ENT,  \
                 KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
-                MO(1),   KC_LGUI, KC_LALT, xxxxxxx, xxxxxxx, KC_SPC,  xxxxxxx, xxxxxxx, xxxxxxx, KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT),
+                MO(1),   KC_LGUI, KC_LALT, xxxxxxx, KC_BSPC, KC_SPC,  xxxxxxx, xxxxxxx, KC_BSPC, KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT),
 
 // layer 1 - functions
         LAYOUT(
-                KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_CALC, \
+                KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  xxxxxxx, \
                 _______, KC_MPRV, KC_MPLY, KC_MNXT, RGB_TOG, BL_TOGG, _______, KC_PGUP, KC_HOME, KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS, KC_DEL,  \
-                _______, _______, KC_VOLD, KC_VOLU, KC_PGDN, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS,  KC_DEL,  xxxxxxx, _______, \
+	        _______, _______, KC_VOLD, KC_VOLU, KC_PGDN, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS,  KC_DEL,  xxxxxxx, _______, \
                 MO(2),   KC_WBAK, KC_WFWD, _______, _______, KC_PGUP, KC_END,  KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY, KC_HOME, KC_PGUP, KC_END,  \
-                MO(1),   _______, _______, xxxxxxx, xxxxxxx, KC_BSPC, xxxxxxx, xxxxxxx, xxxxxxx, _______, _______, KC_MPRV, KC_PGDN, KC_MNXT),
+                _______, _______, _______, xxxxxxx, KC_DEL,  KC_BSPC, xxxxxxx, xxxxxxx, KC_DEL,  MO(2),   _______, KC_MPRV, KC_PGDN, KC_MNXT),
 
 // layer 2 - numpad
         LAYOUT(
-                xxxxxxx, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NLCK, \
-                xxxxxxx, BL_DEC,  BL_INC,  BL_BRTG, xxxxxxx, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, xxxxxxx, xxxxxxx, xxxxxxx, KC_BSPC, _______, \
+                xxxxxxx, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  xxxxxxx, \
+                xxxxxxx, BL_DEC,  BL_INC,  BL_BRTG, RGB_M_R, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, xxxxxxx, xxxxxxx, xxxxxxx, KC_BSPC, KC_NLCK, \
                 xxxxxxx, RGB_RMOD,RGB_MOD, RGB_VAI, RGB_VAD, xxxxxxx, KC_P4,   KC_P5,   KC_P6,   KC_PMNS, xxxxxxx, KC_PEQL, xxxxxxx, _______, \
                 MO(2),   RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, xxxxxxx, KC_P1,   KC_P2,   KC_P3,   KC_PAST, KC_PSLS, xxxxxxx, KC_UP,   xxxxxxx, \
-                MO(1),   xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_P0,   xxxxxxx, xxxxxxx, xxxxxxx, KC_PENT, xxxxxxx, KC_LEFT, KC_DOWN, KC_RGHT),
-
+                MO(1),   xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, KC_P0,   xxxxxxx, xxxxxxx, KC_CALC, KC_PENT, xxxxxxx, KC_LEFT, KC_DOWN, KC_RGHT),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
