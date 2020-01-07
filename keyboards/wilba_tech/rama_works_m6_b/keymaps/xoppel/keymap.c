c693fdc0609c48df5576d564b303bbdb68bf5400
xoppel <16063289+xoppel@users.noreply.github.com>
2020-01-06 20:00:45 -0500

    xoppel M6-B keymap

---
+++ keyboards/wilba_tech/rama_works_m6_b/keymaps/xoppel/keymap.c
@@ -0,0 +1,16 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    LAYOUT(
+        KC_PGUP, KC_UP, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT),
+
+    LAYOUT(
+        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+
+    LAYOUT(
+        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+
+    LAYOUT(
+        KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO) };
+
