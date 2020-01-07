8dfefe014b4bafd35d8b906c92fd6819c6d34d71
xoppel <16063289+xoppel@users.noreply.github.com>
2020-01-06 20:00:45 -0500

    xoppel M6-B keymap

---
+++ keyboards/wilba_tech/rama_works_m6_b/keymaps/xoppel/config.h
@@ -0,0 +1,21 @@
+//#undef RGB_BACKLIGHT_ENABLED
+//#define RGB_BACKLIGHT_ENABLED 0
+
+#undef RGB_BACKLIGHT_BRIGHTNESS
+#define RGB_BACKLIGHT_BRIGHTNESS 40
+#undef RGB_BACKLIGHT_EFFECT
+#define RGB_BACKLIGHT_EFFECT 0
+
+
+#undef VENDOR_ID
+#define VENDOR_ID       0x046D
+#undef PRODUCT_ID
+#define PRODUCT_ID      0xC31C
+#undef DEVICE_VER
+#define DEVICE_VER      0x0064
+#undef MANUFACTURER
+#define MANUFACTURER    Logitech
+#undef PRODUCT
+#define PRODUCT         USB Keyboard
+#undef DESCRIPTION
+#define DESCRIPTION     K120 Keyboard
