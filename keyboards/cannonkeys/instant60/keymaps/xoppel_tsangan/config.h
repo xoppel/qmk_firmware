ee52a40c8fff751e1d604e9876227dd95f41de29
xoppel <16063289+xoppel@users.noreply.github.com>
2020-03-16 11:01:05 -0400

    Update

---
+++ keyboards/cannonkeys/instant60/keymaps/xoppel_tsangan/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#undef VENDOR_ID
+#define VENDOR_ID       0x046D
+#undef PRODUCT_ID
+#define PRODUCT_ID      0xC31C
+#undef DEVICE_VER
+#define DEVICE_VER      0x0064
+#undef MANUFACTURER
+#define MANUFACTURER    Logitech
+#undef PRODUCT
+#define PRODUCT         USB Keyboard
+#undef DESCRIPTION
+#define DESCRIPTION     K120 Keyboard
