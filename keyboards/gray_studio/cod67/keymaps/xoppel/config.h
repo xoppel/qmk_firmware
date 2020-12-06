e3f44182a02cff471d79b2df9af1815bae0311c8
xoppel <16063289+xoppel@users.noreply.github.com>
2020-12-05 21:08:30 -0500

    Remove USB ID spoof from Cod67

--- keyboards/gray_studio/cod67/keymaps/xoppel/config.h
+++ keyboards/gray_studio/cod67/keymaps/xoppel/config.h
@@ -1,18 +1,5 @@
 #include "../../config.h"
 
-#undef VENDOR_ID
-#define VENDOR_ID       0x046D
-#undef PRODUCT_ID
-#define PRODUCT_ID      0xC31C
-#undef DEVICE_VER
-#define DEVICE_VER      0x0064
-#undef MANUFACTURER
-#define MANUFACTURER    Logitech
-#undef PRODUCT
-#define PRODUCT         USB Keyboard
-#undef DESCRIPTION
-#define DESCRIPTION     K120 Keyboard
-
 #define RGB_DI_PIN B2
 #define RGBLED_NUM 20
 #define RGBLIGHT_HUE_STEP 8
