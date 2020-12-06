31b2edf0d0a7475df9a7ff6be0c7bc56434e55c6
xoppel <16063289+xoppel@users.noreply.github.com>
2020-12-05 21:19:08 -0500

    Change Zeal60 RCTRL behavior

--- keyboards/wilba_tech/zeal60/keymaps/xoppel/keymap.c
+++ keyboards/wilba_tech/zeal60/keymaps/xoppel/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
 	KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
 	KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(2),
-	MO(1),   KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RWIN, KC_MENU, KC_RCTL),
+	MO(1),   KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RWIN, KC_MENU, MO(1)),
 
 // Fn1 Layer
 [1] = LAYOUT_60_ansi_split_bs_rshift(
