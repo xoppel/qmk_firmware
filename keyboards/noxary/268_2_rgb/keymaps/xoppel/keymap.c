3700cab3c9faae610a6f586ef4c67ec565d4449e
xoppel <16063289+xoppel@users.noreply.github.com>
2022-08-06 21:03:21 -0400

    Fix bug with 268.2 keymap and first layer

--- keyboards/noxary/268_2_rgb/keymaps/xoppel/keymap.c
+++ keyboards/noxary/268_2_rgb/keymaps/xoppel/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FL1] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_DEL,  KC_CALC,
         _______, KC_MPRV, KC_MPLY, KC_MNXT, RGB_TOG, BL_TOGG, _______, KC_PGUP, KC_HOME, KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS,          _______, KC_VOLU,
-        _______, _______, KC_VOLD, KC_VOLU, KC_PGDN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS,  _______, _______,                   _______, KC_VOLD,
+        _______, _______, KC_VOLD, KC_VOLU, KC_PGDN, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_INS,  _______,                   _______, KC_VOLD,
         _______,          KC_WBAK, KC_WFWD, _______, _______, KC_PGUP, KC_END,  KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY, _______,          KC_PGUP, KC_MUTE,
         _______, _______, _______,                            KC_BSPC,                            MO(_FL2),_______,          KC_MPRV, KC_PGDN, KC_MNXT 
     ),
